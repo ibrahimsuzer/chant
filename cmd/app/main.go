0f15ea1e24c74213eb9b18b42fc406e83eb39ec4
Ibrahim Suzer <ibrahim.suzer@birlesikodeme.com>
2021-05-30 18:34:00 +0300

    add basic flag and cli commands

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,59 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/peterbourgon/ff/v3"
+	"github.com/peterbourgon/ff/v3/ffcli"
 )
 
+var version = "v0.0.0"
+var commit = "00000000"
+
 func main() {
-	fmt.Println("Hello World!")
+	// VERSION
+	version := &ffcli.Command{
+		Name:        "version",
+		ShortUsage:  "",
+		ShortHelp:   "",
+		LongHelp:    "",
+		UsageFunc:   nil,
+		FlagSet:     nil,
+		Options:     []ff.Option{ff.WithEnvVarPrefix("CHANT")},
+		Subcommands: nil,
+		Exec: func(ctx context.Context, args []string) error {
+			fmt.Printf("Chant %v (%v)", version, commit)
+			return nil
+		},
+	}
+
+	// ROOT
+	rootFs := flag.NewFlagSet("chant", flag.ExitOnError)
+	var (
+		verbose = rootFs.Bool("verbose", false, "increase log verbosity")
+	)
+
+	root := &ffcli.Command{
+		Name:        "chant",
+		ShortUsage:  "chant [-version] [-help] [-autocomplete-(un)install] <command> [args]",
+		ShortHelp:   "Chant: Local environment manager for dotfiles and project binaries",
+		LongHelp:    "",
+		UsageFunc:   nil,
+		FlagSet:     rootFs,
+		Options:     []ff.Option{ff.WithEnvVarPrefix("CHANT")},
+		Subcommands: []*ffcli.Command{version},
+		Exec: func(ctx context.Context, args []string) error {
+			fmt.Printf("verbose %v\n", *verbose)
+			return nil
+		},
+	}
+
+	// RUN
+	if err := root.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ibrahimsuzer/chant
 
 go 1.16
+
+require github.com/peterbourgon/ff/v3 v3.0.0
